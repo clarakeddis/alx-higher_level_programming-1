a3c5d8645d7984452e3d3a61208520de3067bcb6
clarakeddis <keddis.clara@gmail.com>
2024-09-11 05:32:29 +0300

    .

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i != 101 and i != 113:
-        print("{:c}".format(i), end="")
+for char in range(97, 123):
+    if (char != 101 and char != 113):
+        print("{:c}".format(char), end='')
